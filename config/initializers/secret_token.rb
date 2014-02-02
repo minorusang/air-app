1221fb66799e2b0a63ea5b26602f42e7fb7fee8f
Song Park <songpark@Songs-MacBook-Air.local>
2014-02-01 18:13:09 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AirApp::Application.config.secret_key_base = '4ab3e5a41f043390cfdae67892204e165422d32a6b737845e808cbacaa47a74b7be72a55597f98f68521d7edb4fc4604ba5b364c3f6295512cb1a2c012de7876'
